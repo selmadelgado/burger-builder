94874b6c6bce58361616aa958fa98ffc2980da69
Selma Delgado <selmadelgado@gmail.com>
2018-10-19 16:11:56 -0500

    118 Removed reduce method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,9 +9,9 @@ const burger = (props) => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
            return <BurgerIngredient key={igKey + i} type={igKey}/>;
         });
-    })
+    });
     
-    .reduce();
+    //.reduce();
 
     console.log(transformedIngredients);
 
