2b2097c23fbc3ac74e1677181afebc4fa80008c1
Selma Delgado <selmadelgado@gmail.com>
2018-10-25 16:31:11 -0500

    127 add UpdatePurchaseState method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,7 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchaseable: false
+        purchasable: false
     }
 
     updatePurchaseState (ingredients) {
@@ -32,7 +32,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) =>{
                 return sum + el; 
             }, 0);
-        this.setState({purchaseable: sum > 0});
+        this.setState({purchasable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -82,7 +82,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler} 
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
-                    purchaseable={this.state.purchaseable}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
                     />
             </Aux>
