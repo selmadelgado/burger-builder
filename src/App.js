b0f0aab4ec62dd302f91c2a13f7de69c2c227a56
Selma Delgado <selmadelgado@gmail.com>
2018-10-30 12:32:50 -0500

    176 Remove Old Interceptors to Prevent Memory Leaks

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
         <Layout>
-         <BurgerBuilder/>
+          <BurgerBuilder/>
         </Layout>
       </div>
     );
